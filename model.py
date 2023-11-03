1a5bbe6ef6c29f737fd6fbb68575a10e3651836b
Kokcr3ator <117846571+Kokcr3ator@users.noreply.github.com>
2023-11-03 19:29:56 +0100

    first submission

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import os
 import tensorflow as tf
+import numpy as np
 
 class model:
     def __init__(self, path):
-        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel\LeNet_CIFAR10'))
+        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel'))
 
     def predict(self, X):
         
@@ -11,5 +12,5 @@ class model:
         # Here the model.predict is called, followed by the argmax
         out = self.model.predict(X)
         out = tf.argmax(out, axis=-1)  # Shape [BS]
-
         return out
+    
